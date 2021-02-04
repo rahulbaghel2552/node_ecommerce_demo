64c442cff515411607f97ec470443fa120cca8f6
rahul baghel <rahulbaghel6513@gmail.com>
2021-02-04 10:36:55 +0530

    file update

---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+console.log("hello from laravel mix 1");
+console.log("hello from laravel mix 2 ");
+console.log("hello from laravel mix 3 ");
+console.log("hello from laravel mix 4");
--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const app = express();
 const ejs = require("ejs");
 const expressLayout = require("express-ejs-layouts");
 const path = require("path");
-
 const PORT = process.env.PORT || 3000;
+
+//assets
+
+const staticPath = path.join(__dirname,'public');
+const resourcesPath = path.join(__dirname,'resources/views');
+console.log(staticPath);
+console.log(resourcesPath);
+
+app.use(express.static(staticPath));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -12,10 +20,11 @@ app.get("/", (req, res) => {
 
 
 // set template engine
-
 app.use(expressLayout);
-app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
+app.set('views',resourcesPath);
+
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
