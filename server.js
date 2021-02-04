405f77333e34855a85f7edc52e23034592f1e6fb
rahul baghel <rahulbaghel6513@gmail.com>
2021-02-04 13:12:15 +0530

    file upadate

--- server.js
+++ server.js
@@ -9,15 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 const staticPath = path.join(__dirname,'public');
 const resourcesPath = path.join(__dirname,'resources/views');
-console.log(staticPath);
-console.log(resourcesPath);
-
 app.use(express.static(staticPath));
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
 
 // set template engine
 app.use(expressLayout);
@@ -25,6 +17,23 @@ app.set('view engine','ejs');
 app.set('views',resourcesPath);
 
 
+
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
